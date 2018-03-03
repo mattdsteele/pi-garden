62ba717c51bb73ba7aeee6240a54581251595527
Matt Steele <orphum@gmail.com>
2018-03-03 11:40:27 -0600

    Back to any.
    PR https://github.com/DefinitelyTyped/DefinitelyTyped/pull/24059

--- soil.ts
+++ soil.ts
@@ -7,8 +7,8 @@ const board: Board = new five.Board({ io: new chip(), repl: false, debug: false 
 
 let prev = 0;
 board.on('ready', () => {
-  const lradc: Pin = new five.Pin({ pin: 'LRADC', type: 'analog' });
-  lradc.read((err, val) => {
+  const lradc: any = new five.Pin({ pin: 'LRADC', type: 'analog' });
+  lradc.read((err: Error, val: number) => {
     if (val !== prev) {
       console.log(val);
       prev = val;
