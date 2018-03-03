79b32728cd6c6611fb69273e924c3ca807ee29b5
mattdsteele <orphum@gmail.com>
2018-03-03 17:33:13 +0000

    bug in typings :(

--- soil.ts
+++ soil.ts
@@ -8,7 +8,7 @@ const board: Board = new five.Board({ io: new chip(), repl: false, debug: false 
 let prev = 0;
 board.on('ready', () => {
   const lradc: Pin = new five.Pin({ pin: 'LRADC', type: 'analog' });
-  lradc.read(val => {
+  lradc.read((err, val) => {
     if (val !== prev) {
       console.log(val);
       prev = val;
